a71b6c994e8420d3dede938d82e63ff2f01160be
lgdd <louis-guillaume.durand@liferay.com>
2021-05-07 06:33:46 +0200

    feat: add self-update

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/lgdd/go-gh-actions
 
 go 1.16
+
+require (
+	github.com/blang/semver v3.5.1+incompatible
+	github.com/rhysd/go-github-selfupdate v1.2.3
+)
--- main.go
+++ main.go
@@ -1,6 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/blang/semver"
+	"github.com/rhysd/go-github-selfupdate/selfupdate"
+)
 
 var (
 	version string
@@ -9,7 +16,42 @@ var (
 )
 
 func main() {
-	fmt.Printf("Version: %s\n", version)
-	fmt.Printf("Commit: %s\n", commit)
-	fmt.Printf("Date: %s\n", date)
+	args := os.Args[1:]
+	fmt.Printf("%v\n", args)
+	if len(args) == 1 {
+		cmd := args[0]
+		if cmd == "version" {
+			fmt.Printf("Version: %s\n", version)
+			fmt.Printf("Commit: %s\n", commit)
+			fmt.Printf("Date: %s\n", date)
+		} else if cmd == "update" {
+
+		} else {
+			fmt.Println("unknown command")
+			os.Exit(1)
+		}
+	} else if len(args) == 0 {
+		fmt.Println("try one of the commands:")
+		fmt.Println("- version")
+		fmt.Println("- update")
+	} else {
+		fmt.Println("unknown command")
+		os.Exit(1)
+	}
+}
+
+func doSelfUpdate() {
+	v := semver.MustParse(version)
+	latest, err := selfupdate.UpdateSelf(v, "lgdd/go-gh-actions")
+	if err != nil {
+		log.Println("Binary update failed:", err)
+		return
+	}
+	if latest.Version.Equals(v) {
+		// latest version is the same as current version. It means current binary is up to date.
+		log.Println("Current binary is the latest version", version)
+	} else {
+		log.Println("Successfully updated to version", latest.Version)
+		log.Println("Release note:\n", latest.ReleaseNotes)
+	}
 }
